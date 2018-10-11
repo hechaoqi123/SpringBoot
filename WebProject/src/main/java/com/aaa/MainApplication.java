54b64441e280d2ce191643fa81c2547f16b36209
Administrator <Administrator@PC-201710152136>
2018-10-11 10:49:51 +0800

    Test

--- WebProject/src/main/java/com/aaa/MainApplication.java
+++ WebProject/src/main/java/com/aaa/MainApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.SpringBootConfiguration;
 public class MainApplication {
 	public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
+        String a="a";
     }
 }
